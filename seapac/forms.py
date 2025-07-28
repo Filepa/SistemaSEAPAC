c4777e9cc5b3459d16d5091ab91413f11250127e
Filepa <feliperaposo07@gmail.com>
2025-07-28 16:49:00 -0300

    atualizações no forms

--- seapac/forms.py
+++ seapac/forms.py
@@ -10,7 +10,13 @@ class FamilyForm(ModelForm):
         widgets = {
             'nome_titular' : forms.TextInput(attrs={'class': 'form-control' }),
             'cpf' : forms.TextInput(attrs={'class': 'form-control' }),
-            'bpc' : forms.TextInput(attrs={'class': 'form-control' }),
-            'nis': forms.TextInput(attrs={'class': 'form-control' }),
-            'dap': forms.TextInput(attrs={'class': 'form-control' }),
+            'data_nasc': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}, format='%Y-%m-%d'),
+            'data_inic': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}, format='%Y-%m-%d'),
+            'contato': forms.TextInput(attrs={'class': 'form-control',}),
+            'bpc': forms.TextInput(attrs={'class': 'form-control'}),
+            'nis': forms.TextInput(attrs={'class': 'form-control'}),
+            'dap': forms.TextInput(attrs={'class': 'form-control'}),
+            'aposentadoria': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+            'auxilio': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+            'escolaridade': forms.Select(attrs={'class': 'form-select'}),
         }
