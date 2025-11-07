e38dbdc9ad771dc96997897f85765ad9eb265a2b
Anamarela <rafaelanamaria271@gmail.com>
2025-11-07 20:31:24 -0300

    Implementação da Homepage completa

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
+    path('home/', TemplateView.as_view(template_name='homepage/principal.html'), name='home'),
     path('cadastrar-user/', views.cadastrar_usuario, name='cadastrar_usuario'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
