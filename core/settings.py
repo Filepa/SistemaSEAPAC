ccca28c651f4cded2cf24ff74f8b29f28e2a3864
Felipe Raposo <feliperaposo07@gmail.com>
2025-12-02 07:43:39 -0300

    settings do email

--- core/settings.py
+++ core/settings.py
@@ -154,3 +154,13 @@ AUTH_USER_MODEL = "usuarios.Usuario"
 LOGIN_URL = "login"
 LOGIN_REDIRECT_URL = "index"
 LOGOUT_REDIRECT_URL = "login"
+
+#Recursos para recuperação de senha pelo próprio Django
+EMAIL_BACKEND = 'django.core.email.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+EMAIL_HOST_USER = "seapac@gmail.com"
+EMAIL_HOST_PASSWORD = "havg xqfl jkas itus"
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
