89e13fd23ef193d3e46d1c2c716dcd2e935f5c56
Filepa <feliperaposo07@gmail.com>
2025-12-05 12:53:34 -0300

    email corrigido

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
         "reset_password/",
         auth_views.PasswordResetView.as_view(
             template_name="registration/password_reset_form.html",
-            email_template_name="registration/password_reset_email.html",
+            email_template_name="registration/password_reset_email.txt",
         ),
         name="reset_password",
     ),
